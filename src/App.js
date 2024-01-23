701a4961b49f2e8c82f7adaa801dd45f0660873f
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2024-01-23 17:24:09 -0500

    Order Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState("")
+  const [pepperoni, setPepperoni] = useState(false)
+  const [mushrooms, setMushrooms] = useState(false)
+  const [olives, setOlives] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+      <header>Pizza Order</header>
+        <label>
+          Name:
+          <input className="Name-field"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={pepperoni}
+            onChange={() => setPepperoni(!pepperoni)}
+          />
+          Pepperoni
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={mushrooms}
+            onChange={() => setMushrooms(!mushrooms)}
+          />
+          Mushrooms
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={olives}
+            onChange={() => setOlives(!olives)}
+          />
+          Olives
+        </label>
+        <button type="submit">Order Now</button>
+      </form>
+
+      <div className="summary">
+        <h2>Order Summary</h2>
+        <p>Name: {name}</p>
+        {pepperoni && <p>Pepperoni</p>}
+        {mushrooms && <p>Mushrooms</p>}
+        {olives && <p>Olives</p>}
+      </div>
     </div>
   );
 }
